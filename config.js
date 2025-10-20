1f5160f2a58067140a044ff9e9103ce68cb14f4b
Harindu De Silva <harindudesilva12@gmail.com>
2025-10-20 21:07:38 +0530

    ev updated for auth service

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const config = {
   port: process.env.PORT || 8080,
-  authServiceUrl: process.env.AUTH_SERVICE_URL || "http://localhost:5000",
+  authServiceUrl: process.env.AUTH_SERVICE_URL || "https://predict4-life-auth-service.vercel.app",
   mlServiceUrl: process.env.ML_SERVICE_URL || "http://localhost:8000",
   jwtSecret: process.env.JWT_SECRET || "supersecret123",
 };
