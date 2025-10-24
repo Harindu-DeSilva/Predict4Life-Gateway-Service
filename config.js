ff1cccd20175cc6801e2b8ddff2e882db862277c
Harindu De Silva <harindudesilva12@gmail.com>
2025-10-24 16:24:31 +0530

    donor routes added with config

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ export const config = {
   port: process.env.PORT || 8080,
   authServiceUrl: process.env.AUTH_SERVICE_URL || "http://localhost:5000",
   mlServiceUrl: process.env.ML_SERVICE_URL || "http://localhost:8000",
-  donorServiceUrl: process.env.DONOR_SERVICE_URL || "predict4-life-donor-service.vercel.app",
+  donorServiceUrl: process.env.DONOR_SERVICE_URL || "https://predict4-life-donor-service.vercel.app",
   jwtSecret: process.env.JWT_SECRET || "supersecret123",
 };
